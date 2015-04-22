e591dee041e878581c08eaa1ba1fd539ba9c4e3a
Emil Stenström <em@kth.se>
2015-04-22 10:01:26 +0200

    First version av API.

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+setup(
+    name='nutshell',
+    packages=['nutshell'],
+    version='0.1',
+    description='A minimal python library to access Nutshell CRM:s JSON-RPC API.',
+    author=u'Emil Stenström',
+    author_email='em@kth.se',
+    url='https://github.com/EmilStenstrom/python-nutshell',
+    download_url='https://github.com/EmilStenstrom/python-nutshell/tarball/0.1',
+    keywords=['nutshell', 'nutshell-crm', 'json-rpc'],
+    classifiers=[],
+)
